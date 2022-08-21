c58c747d0334423c5aa6aec166227ddce50ce5b8
Ayodeji O <osasonaayodeji1@gmail.com>
2022-08-21 03:15:13 +0100

    Styled form

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ use Trulyao\PhpRouter\HTTP\Request as Request;
 
 $router = new Router(__DIR__ . "/src", "");
 
+$router->allowed(["text/html", "multipart/form-data", "application/x-www-form-urlencoded"]);
+
 $router->get("/", function (Request $request, Response $response) {
     return $response->use_engine()->render("Views/home.html", $request);
 });
